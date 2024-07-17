fbd1285d81c9b73944e87d2a2649e3f0b8d04da6
akhilworks2001 <112404416+akhilworks2001@users.noreply.github.com>
2024-07-17 17:34:23 +0530

    testing sign in and log in

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,22 +4,22 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const generateAccessAndRefreshTokens = async (userId) => {
+const generateAccessAndRefereshTokens = async(userId) =>{
     try {
         const user = await User.findById(userId)
-        const accessToken = user.generateAccessToken();
-        const refreshToken = user.generateRefreshToken();
-        
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
         user.refreshToken = refreshToken
         await user.save({ validateBeforeSave: false })
 
         return {accessToken, refreshToken}
 
+
     } catch (error) {
-        throw new ApiError(500, 'Something went wrong while generating and refresh and access token')
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
     }
 }
-
 const registerUser = asyncHandler ( async ( req, res ) => {
     // get user details from frontend
     // validation - not empty
@@ -33,6 +33,7 @@ const registerUser = asyncHandler ( async ( req, res ) => {
 
     const { fullname, email, username, password } = req.body
     console.log("fullname: ", fullname);
+    console.log("username: ", username);
 
     if (
         [fullname, email, username, password].some((field) => field?.trim() === "")
@@ -53,7 +54,6 @@ const registerUser = asyncHandler ( async ( req, res ) => {
     // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
-    console.log(req.files)
 
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
@@ -100,10 +100,12 @@ const loginUser = asyncHandler (async (req, res) => {
     // send cookie 
     // if not rec registeruser
 
-    const { email, username, password } = req.body
+    const { email, username, password } = req.body;
+
+    console.log(email, username, password)
     
-    if (!username || !email) {
-        throw new ApiError(400, "Username or password is required")
+    if (!(username || email)) {
+        throw new ApiError(400, "Username or email is required")
     };
 
     const user = await User.findOne({
@@ -114,20 +116,21 @@ const loginUser = asyncHandler (async (req, res) => {
         throw new ApiError(404, "User does not exist")
     };
 
-    const isPasswordValid = await user.isPasswordCorrect(password);
+    const isPasswordValid = await user.isPasswordCorrect(String(password));
+
 
     if (!isPasswordValid) {
         throw new ApiError(404, "Invalid user credentials")
     }
 
-    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id);
 
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
-    const options = {
+    const options = { 
         httpOnly: true,
         secure: true
-    }
+    } // Now it can only modify by the server and frontend only can see it and cannot modiy it
 
     return res.
         status(200).
@@ -166,7 +169,7 @@ const logoutUser = asyncHandler (async(req, res) => {
     status(200)
     .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
-    .json(new ApiResponse(299, {}, "User logged Out"))
+    .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
 
