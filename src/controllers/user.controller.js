abd5cd617f36782b1d9a2bb1f10124ba6baeb14b
akhilworks2001 <112404416+akhilworks2001@users.noreply.github.com>
2024-07-23 13:59:09 +0530

    add refresh token end point

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefereshTokens = async(userId) =>{
     try {
@@ -20,6 +21,7 @@ const generateAccessAndRefereshTokens = async(userId) =>{
         throw new ApiError(500, "Something went wrong while generating referesh and access token")
     }
 }
+
 const registerUser = asyncHandler ( async ( req, res ) => {
     // get user details from frontend
     // validation - not empty
@@ -132,19 +134,19 @@ const loginUser = asyncHandler (async (req, res) => {
         secure: true
     } // Now it can only modify by the server and frontend only can see it and cannot modiy it
 
-    return res.
-        status(200).
-        cookie("accessToken", accessToken, options).
-        cookie("refreshToken", refreshToken, options).
-        json(
-            new ApiResponse (
-                200,
-                {
-                    user: loggedInUser, accessToken, refreshToken
-                },
-                "User loggend In Successfully"
-            )
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, )
+    .cookie("refreshToken", refreshToken, )
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
         )
+    )
 
 });
 
@@ -172,5 +174,50 @@ const logoutUser = asyncHandler (async(req, res) => {
     .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
+const refreshAccessToken = asyncHandler (async (req, res) => {
+    try {
+        const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-export { registerUser, loginUser, logoutUser };
+    if (incomingRefreshToken) {
+        throw new ApiError (401, "unauthorized request")
+    }
+
+    const decodedToken = jwt.verify (
+        incomingRefreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+    )
+
+    const user = await User.findById(decodedToken?._id)
+
+        if (!user ) {
+            throw new ApiError(401, "invalid refresh token")
+        }
+
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError (401, "Refresh token is expired or used")
+        }
+
+        const options  = {
+            httpOnly: true,
+            secure:true
+        }
+
+        const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {accessToken, refreshToken: newRefreshToken},
+                "Access Token refreshed"
+            )
+        )
+    } catch (error) {
+        throw new ApiError (401, error?.message || "Invaid refresh token")
+    }
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,65 +1,68 @@
+
 import mongoose, {Schema} from "mongoose";
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-    },
-    fullname: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true
-    },
-    avatar: {
-        type: String, // cloudinary url ( it's an AWS where we can store files or images )
-        required: true,
-    },
-    coverImage: {
-        type: String, // cloudinary url ( it's an AWS where we can store files or images )
-    },
-    watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Video"
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
         }
-    ],
-    password: {
-        type: String,
-        required: [true, "Password is required"]
-    },
-    refreshToken: {
-        type: String
-    }
 
-}, {
-    timestamps: true
-}
+    },
+    {
+        timestamps: true
+    }
 )
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+    if(!this.isModified("password")) return next();
 
     this.password = await bcrypt.hash(this.password, 10)
-    next();
+    next()
 })
 
-userSchema.methods.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
-};
+}
 
 userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
@@ -88,6 +91,4 @@ userSchema.methods.generateRefreshToken = function(){
     )
 }
 
-
-
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema)
