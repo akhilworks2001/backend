5b2417399d3cdb235e0304654627c7b1d81bcdfc
akhilworks2001 <112404416+akhilworks2001@users.noreply.github.com>
2024-07-28 13:50:58 +0530

    Add comment, like, playlist and tweet models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -449,60 +449,58 @@ const getUserChannelProfile = asyncHandler (async (req, res)  => {
  
 })
 
-const getWatchHistory = asyncHandler(async (req, res) => {
+const getWatchHistory = asyncHandler(async(req, res) => {
   const user = await User.aggregate([
-    {
-      $match: {
-        _id: new mongoose.Types.ObjectdId(req.user?._id)
-      }
-    },
-    {
-      $lookup: {
-        from: "videos",
-        localField: "watchHistory",
-        foreignField: "_id",
-        as: "watchHistory",
-        pipeline: [
-          {
-            $lookup: {
-              from: "users",
-              localField: "owner",
-              foregnField: "_id",
-              as: "owner",
-              pipeline: [
-                {
-                  $projct: {
-                    fullName: 1,
-                    username: 1,
-                    avatar: 1
-                  }
-                }
-              ]
-            }
-          },
-          {
-            $addFields: {
-              owner: {
-                $first: "$owner"
-              }
-            }
+      {
+          $match: {
+              _id: new mongoose.Types.ObjectId(req.user._id)
           }
-        ]
+      },
+      {
+          $lookup: {
+              from: "videos",
+              localField: "watchHistory",
+              foreignField: "_id",
+              as: "watchHistory",
+              pipeline: [
+                  {
+                      $lookup: {
+                          from: "users",
+                          localField: "owner",
+                          foreignField: "_id",
+                          as: "owner",
+                          pipeline: [
+                              {
+                                  $project: {
+                                      fullName: 1,
+                                      username: 1,
+                                      avatar: 1
+                                  }
+                              }
+                          ]
+                      }
+                  },
+                  {
+                      $addFields:{
+                          owner:{
+                              $first: "$owner"
+                          }
+                      }
+                  }
+              ]
+          }
       }
-    }
-
   ])
 
   return res
   .status(200)
   .json(
-    new ApiResponse(
-      200,
-      user[0].watchHistory,
-      "Watch history fetched successfully"
-    )
+      new ApiResponse(
+          200,
+          user[0].watchHistory,
+          "Watch history fetched successfully"
+      )
   )
-
 })
 
 export { 
